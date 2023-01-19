38a613e6025f5ebfa3d377cc03ad4c7b9f3f43cf
Kiran Kumar K <kirankumarwaste1@gmail.com>
2023-01-19 17:32:37 +0530

    build

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e3367f730bb53eb"; }
+/******/ 		__webpack_require__.h = function() { return "f30ae934e2ce7834"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
